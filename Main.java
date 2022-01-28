d2f54abec2efd1aff4b2cbfb1e7a160d0c7e0e48
Phos15 <56139315+Phos202@users.noreply.github.com>
2022-01-28 13:30:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+      Scanner scan = new Scanner(System.in);
+
+
+System.out.println("Welcome to your easy java learning course,\nHere You Will begin with learning the basics of math !WHEN YOU WANT TO MOVE ON TO THE NEXT LESSON YOU WILL BE PROMTED WHEN!\nIf you want to practice go to Replit.com");
+
+  
+   
+
+/* 
+The number 1 is to refer to notes on how ints and doubles work
+
+The number 2 is for a begining lesson on strings
+*/
+
+}
+
+    
+} // dont delete this line
---
+++ Topic.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+
+public class Topic
+{
+      
+private int selc;
+Scanner scan = new Scanner(System.in);
+  
+
+
+    public void tNotes(String[] tname, String[] notes)
+    { 
+      System.out.println("\nSelect a lesson/note(0-4)");
+      selc = scan.nextInt(); 
+
+    }
+ 
+      //     public String toString()
+      //     {
+      //     return "This is " + tname[selc] +notes[selc];
+      //     }
+  
+  
+  
+  
+
+}
