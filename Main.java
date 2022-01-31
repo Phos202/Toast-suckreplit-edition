c4dbc3a1ce7875cbe97c6c1e2d2360cebc182ba0
Phos15 <56139315+Phos202@users.noreply.github.com>
2022-01-31 13:10:07 +0000

    grammar in intb

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
       Scanner scan = new Scanner(System.in);
 
-Topic intb = new Topic("Ints","When working with ints they should probably be written as  int *name* = *whatever value you want*;  \n\t (NEVER FORGET THE SMICOLIN )\n Things you can't do:\n\nInts hold a total byte data of 32 and cant hold the extra parts of a decimal\n\n If you intend to put a double in a int like 6.5,/n it would become 6 cause a int cant hold a byte size that is bigger than its own.\n___________________________________\n\n\n");
+Topic intb = new Topic("Ints","When working with ints they should probably be written as  int *name* = *whatever value you want*;  \n\t(NEVER FORGET THE SMICOLIN)\n Things you can't do:\n\nInts hold a total byte data of 32 and cant hold the extra parts of a decimal\n\n If you intend to put a double in a int like 6.5, it would become 6 cause a int cant hold a byte size that is bigger than its own.\n___________________________________\n\n\n");
 System.out.println(intb);
 
 Topic doubleb = new Topic("Doubles","");
